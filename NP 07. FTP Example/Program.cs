ca5f8bc13a056d4a057ae77e7011078ab48ceb6c
Nadir Zamanov <123265575+Zamanof@users.noreply.github.com>
2024-10-18 07:46:13 +0000

    ftp example

---
+++ NP 07. FTP Example/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+
+getFTP();
+//DownloadFTP();
+//UploadFTP();
+void getFTP()
+{
+    var request = WebRequest.Create("ftp://localhost:21") as FtpWebRequest;
+
+    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+    var sr = new StreamReader(stream);
+    var data = sr.ReadToEnd();
+    Console.WriteLine(data);
+}
+
+void DownloadFTP()
+{
+    var request = WebRequest.Create("ftp://localhost:21/Exam.txt") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+    var fileStream = new FileStream("supperpuppermupper.txt", FileMode.Create);
+
+    stream.CopyTo(fileStream);
+    fileStream.Close();
+    stream.Close();
+
+}
+
+void UploadFTP()
+{
+    var request = WebRequest.Create("ftp://localhost:21/Winter.txt") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.UploadFile;
+
+    using var fs = new FileStream("supperpuppermupper.txt", FileMode.Open);
+    using var stream = request.GetRequestStream();
+    fs.CopyTo(stream);
+}
