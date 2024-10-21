966ba6bb9fc9335bb995844d69ddd26fe307dd05
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-10-21 11:00:05 +0400

    MultiClient TCP

---
+++ NP 08. Tcp Multi Client Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var client = new TcpClient();
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+
+try
+{
+    client.Connect(endPoint);
+    if (client.Connected)
+    {
+        var stream = client.GetStream();
+        var bw = new BinaryWriter(stream);
+        var br = new BinaryReader(stream);
+        var writer = Task.Run(() =>
+        {
+            while (true)
+            {
+                var text = Console.ReadLine();
+                bw.Write(text);
+            }
+        });
+
+        var reader = Task.Run(() =>
+        {
+            while (true)
+            {
+                Console.WriteLine($"From server: {br.ReadString()}");
+            }
+        });
+        Task.WaitAll(writer, reader);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
---
+++ NP 08. Tcp Multi Client Listener/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Concurrent;
+
+TcpListener listener = null;
+BinaryReader br = null;
+BinaryWriter bw = null;
+List<TcpClient> clients = [];
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+listener = new TcpListener(endPoint);
+
+listener.Start(10);
+
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    clients.Add(client);
+    Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+    var reader = Task.Run(() => {
+        try
+        {
+            while (true)
+            {
+                var stream = client.GetStream();
+                br = new BinaryReader(stream);
+                var message = br.ReadString();
+                Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {message}");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine($"{client.Client.RemoteEndPoint} disconnected.");
+            clients.Remove(client);
+        }
+    });
+
+    var writer = Task.Run(() => {
+        while (true)
+        {
+            var message = Console.ReadLine();
+            foreach (var client in clients)
+            {
+                var stream = client.GetStream();
+                bw = new BinaryWriter(stream);
+                bw.Write(message);
+            }
+        }
+    });
+}
